2885f5a840386128df99eb8559345fcb255ced17
Marcell Monteiro Cruz <0000marcell@gmail.com>
2018-06-01 16:34:13 -0300

    watch functionality added

--- figma2react.js
+++ figma2react.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 
 const baseUrl = 'https://api.figma.com';
 
+
+
 const vectorMap = {};
 const vectorList = [];
 const vectorTypes = ['VECTOR', 'LINE', 'REGULAR_POLYGON', 'ELLIPSE', 'STAR'];
@@ -72,6 +74,82 @@ function preprocessTree(node) {
   }
 }
 
+async function generateComponents(data) {
+  const doc = data.document;
+  const canvas = doc.children[0];
+  const config = data.config;
+  const headers = data.headers;
+
+  let html = '';
+
+  for (let i=0; i<canvas.children.length; i++) {
+    const child = canvas.children[i]
+    if (child.name.charAt(0) === '#'  && child.visible !== false) {
+      const child = canvas.children[i];
+      preprocessTree(child);
+    }
+  }
+
+  let guids = vectorList.join(',');
+  data = await fetch(`${baseUrl}/v1/images/${config.projectId}?ids=${guids}&format=svg`, {headers});
+  const imageJSON = await data.json();
+
+  const images = imageJSON.images || {};
+  if (images) {
+    let promises = [];
+    let guids = [];
+    for (const guid in images) {
+      if (images[guid] == null) continue;
+      guids.push(guid);
+      promises.push(fetch(images[guid]));
+    }
+
+    let responses = await Promise.all(promises);
+    promises = [];
+    for (const resp of responses) {
+      promises.push(resp.text());
+    }
+
+    responses = await Promise.all(promises);
+    for (let i=0; i<responses.length; i++) {
+      images[guids[i]] = responses[i].replace('<svg ', '<svg preserveAspectRatio="none" ');
+    }
+  }
+
+  const componentMap = {};
+  
+  for (let i=0; i<canvas.children.length; i++) {
+    const child = canvas.children[i]
+    if (child.name.charAt(0) === '#' && child.visible !== false) {
+      const child = canvas.children[i];
+      figma.createComponent(child, images, componentMap);
+    }
+  }
+
+  for (const key in componentMap) {
+    let component = componentMap[key];
+    let contents = "import React, { Component} from 'react'\n";
+    contents+="\n";
+    contents += component.doc + "\n";
+    const path = `./src/components/${component.name}.js`;
+    fs.writeFile(path, contents, function(err) {
+      if (err) console.log(err);
+      console.log(`wrote ${path}`);
+    });
+  }
+}
+
+async function fetchProject() {
+  let config = JSON.parse(fs.readFileSync('./.figma2react'));
+  headers.set('X-Figma-Token', config.token);
+  let resp = await fetch(`${baseUrl}/v1/files/${config.projectId}`, 
+    {headers});
+  let data = await resp.json();
+  data['config'] = config;
+  data['headers'] = headers;
+  return data;
+}
+
 program
   .version('0.1.3')
   .description('Generates react components from figmas designs');
@@ -82,74 +160,27 @@ program
   .description('generate react component')
   .option("-c, --component [name]", "component name")
   .action(async () => {
-    let config = JSON.parse(fs.readFileSync('./.figma2react'));
+    let data = await fetchProject();
+    generateComponents(data);
+  });
 
-    headers.append('X-Figma-Token', config.token);
-
-    let resp = await fetch(`${baseUrl}/v1/files/${config.projectId}`, 
-      {headers});
-
-    let data = await resp.json();
-
-    const doc = data.document;
-    const canvas = doc.children[0];
-    let html = '';
-
-    for (let i=0; i<canvas.children.length; i++) {
-      const child = canvas.children[i]
-      if (child.name.charAt(0) === '#'  && child.visible !== false) {
-        const child = canvas.children[i];
-        preprocessTree(child);
+program
+  .command('watch')
+  .alias('w')
+  .description('watch for changes in the figma projects and generate the components')
+  .action(async () => {
+    let data = await fetchProject(),
+      currentDate = data.lastModified;
+    setInterval(async () => {
+      data = await fetchProject();
+      if(data.lastModified > currentDate) {
+        generateComponents(data);
+        currentDate = data.lastModified;
+        console.log(`project changed, making modifications!`);
       }
-    }
-
-    let guids = vectorList.join(',');
-    data = await fetch(`${baseUrl}/v1/images/${config.projectId}?ids=${guids}&format=svg`, {headers});
-    const imageJSON = await data.json();
-
-    const images = imageJSON.images || {};
-    if (images) {
-      let promises = [];
-      let guids = [];
-      for (const guid in images) {
-        if (images[guid] == null) continue;
-        guids.push(guid);
-        promises.push(fetch(images[guid]));
-      }
-
-      let responses = await Promise.all(promises);
-      promises = [];
-      for (const resp of responses) {
-        promises.push(resp.text());
-      }
-
-      responses = await Promise.all(promises);
-      for (let i=0; i<responses.length; i++) {
-        images[guids[i]] = responses[i].replace('<svg ', '<svg preserveAspectRatio="none" ');
-      }
-    }
-
-    const componentMap = {};
-    
-    for (let i=0; i<canvas.children.length; i++) {
-      const child = canvas.children[i]
-      if (child.name.charAt(0) === '#' && child.visible !== false) {
-        const child = canvas.children[i];
-        figma.createComponent(child, images, componentMap);
-      }
-    }
-
-    for (const key in componentMap) {
-      let component = componentMap[key];
-      let contents = "import React, { Component} from 'react'\n";
-      contents+="\n";
-      contents += component.doc + "\n";
-      const path = `./src/components/${component.name}.js`;
-      fs.writeFile(path, contents, function(err) {
-        if (err) console.log(err);
-        console.log(`wrote ${path}`);
-      });
-    }
+    }, 1000);
+    console.log(`watching figma project ${data.config.projectId}`);
+    console.log(`last modified ${currentDate}`);
   });
 
 program.parse(process.argv);
---
+++ test.js
@@ -0,0 +1,7 @@
+var string1 = "2018-06-01T17:55:52.717Z";
+var string2 = "2018-06-01T17:25:24.02z";
+if(string1 > string2){
+  console.log('string1 bigger');
+}else{
+  console.log('string2 bigger');
+}
